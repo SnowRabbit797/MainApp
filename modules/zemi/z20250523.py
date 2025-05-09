f214ec9e8a83947652cc51bd739669154e55c90c
SnowRabbit797 <aoi01711@icloud.com>
2025-05-09 16:54:04 +0900

    config.tomlファイルを追加しました

--- modules/zemi/z20250523.py
+++ modules/zemi/z20250523.py
@@ -10,3 +10,19 @@ def main():
 
     if section == "section1":
         st.title("2025年5月23日 M2ゼミ発表(3回目)")
+    elif section == "section2":
+        st.title("(仮)グラフの作成")
+        
+        G = nx.Graph()
+        G.add_edges_from([(1, 2), (1, 3), (3, 4), (2, 4), (1, 4)])
+
+        pos = {
+          1: (0, 0),
+          2: (1, 0),
+          3: (0, -1),
+          4: (1, -1)
+        }
+        fig, ax = plt.subplots()
+        nx.draw(G, pos, with_labels=True, node_color="lightblue", edge_color="gray", node_size=700, font_size=12, ax=ax)
+        
+        st.pyplot(fig)
