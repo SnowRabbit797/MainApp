76af5f108228c43b119813c5c01c3c6b43c239f0
SnowRabbit797 <aoi01711@icloud.com>
2025-05-30 14:22:08 +0900

    排他処理デモのページの完成

--- pages/7_Experiment.py
+++ pages/7_Experiment.py
@@ -2,7 +2,7 @@ import streamlit as st
 from filelock import FileLock, Timeout
 import time
 
-lock_path = "my_task.lock"
+lock_path = "demo.lock"
 
 st.title("排他制御デモ")
 
@@ -15,3 +15,25 @@ if st.button("処理を開始"):
             st.success("処理が完了しました")
     except Timeout:
         st.warning("他のユーザーが現在処理中です。")
+
+with st.container(border = True):
+    st.subheader("コード", divider="orange")
+    st.code("""
+        import streamlit as st
+        from filelock import FileLock, Timeout
+        import time
+
+        lock_path = "demo.lock"
+
+        st.title("排他制御デモ")
+
+        if st.button("処理を開始"):
+            st.write("[処理を開始]を押すと、10秒間擬似的な処理をしているようなプログラムが実行されます。")
+            try:
+                with FileLock(lock_path, timeout=1):
+                    st.success("ロックを取得しました。処理中です...")
+                    time.sleep(10)
+                    st.success("処理が完了しました")
+            except Timeout:
+                st.warning("他のユーザーが現在処理中です。")
+    """)
